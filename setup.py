fa9fa5a7b95e56543e6cb3e4653522a578fa2e9f
Joe Yesselman <jyesselm@unl.edu>
2022-01-04 12:42:43 -0600

    fixed minor bug in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-readme = open('README.rst').read()
+readme = open('README.md').read()
 doclink = """
 Documentation
 -------------
